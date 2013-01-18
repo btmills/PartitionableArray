77e46c7a7c4b97fb5bba0534e32206dbb70ae394
Brandon Mills <mills.brandont@gmail.com>
2013-01-18 03:28:44 -0500

    Use .NET CodeContracts
    - Added contracts
    - Removed PartitionableArray.IsInteresting() - made redundant by
    PartitionableArray.Count
    - Changed how sentinel and Length are calculated in PartitionableArray
    - To test if interesting initial values are considered, allowed 0 in
    check function

--- C#/PartitionableArray/PartitionableArray.cs
+++ C#/PartitionableArray/PartitionableArray.cs
@@ -1,16 +1,12 @@
 using System;
-//using System.Collections.Generic;
-//using System.Linq.Expressions;
+using System.Diagnostics.Contracts;
 
 namespace PartitionableArray
 {
 	public class PartitionableArray<T>
 	{
-		#region Internal representation
+		#region Internal
 
-		/// <summary>
-		/// Representation of an element in the array.
-		/// </summary>			
 		private struct Element
 		{
 			public T value;
@@ -21,49 +17,91 @@ namespace PartitionableArray
 			public int next;
 		}
 
-		/// <summary>
-		/// Internal array representation.
-		/// </summary>
 		private Element[] arr;
 
-		/// <summary>
-		/// List of interesting elements.
-		/// </summary>
-		private int sentinel;
-		
-		/// <summary>
-		/// Stored partition function.
-		/// </summary>
 		private PartitionFn test;
+
+		private int sentinel
+		{
+			get { return arr.Length - 1; }
+		}
+
+		[ContractInvariantMethod]
+		private void invariant()
+		{
+			Contract.Invariant(test != null);
+			Contract.Invariant(arr != null);
+			Contract.Invariant(arr.Length >= 1);
+			Contract.Invariant(sentinel == arr.Length - 1);
+		}
+
+		private void consider(int index)
+		{
+			Contract.Requires<IndexOutOfRangeException>(
+				0 <= index && index < Length,
+				"The index must be within the bounds of the array.");
+			Contract.Assume(index < arr.Length);
+
+			if (test(arr[index].value)) // Is interesting
+			{
+				if (!arr[index].interesting) // Is not in list
+				{
+					// Add to list
+					count++;
+					arr[index].interesting = true;
+
+					arr[index].next = sentinel;
+					arr[index].prev = arr[sentinel].prev;
+					arr[arr[sentinel].prev].next = index;
+					arr[sentinel].prev = index;
+				}
+				// Else already in list, no need to add
+			}
+			else // Is not interesting
+			{
+				if (arr[index].interesting) // Is in list
+				{
+					// Remove from list
+					count--;
+					arr[index].interesting = false;
+
+					arr[arr[index].prev].next = arr[index].next;
+					arr[arr[index].next].prev = arr[index].prev;
+				}
+				// Else is not already in list, no need to remove
+			}
+		}
 
 		#endregion
 
-		#region Interface
+		#region Public
 
 		/// <summary>
 		/// Given a value, decide if it is interesting.
 		/// </summary>
+		[Pure]
 		public delegate bool PartitionFn(T val);
 
-		private int length = 0;
 		/// <summary>
 		/// Gets maximum the number of elements in the array.
 		/// </summary>
 		/// <value>
 		/// The maximum number of elements in the array.
 		/// </value>
-		public int Length {
-			get { return length; }
+		public int Length
+		{
+			get { return arr.Length - 1; }
 		}
 
-		private int count = 0;
+		int count = 0;
 		/// <summary>
 		/// Gets the number of interesting elements in the array.
 		/// </summary>
 		/// <value>
 		/// The number of interesting elements in the array.
 		/// </value>
-		public int Count {
+		public int Count
+		{
 			get { return count; }
 		}
 
@@ -77,21 +115,21 @@ namespace PartitionableArray
 		/// <param name='length'>
 		/// The fixed maximum number of elements in the array.
 		/// </param>
-		public PartitionableArray (PartitionFn fn, int length)
+		public PartitionableArray(PartitionFn fn, int length)
 		{
-			this.test = fn;
-			this.length = length;
-			arr = new Element[length + 1]; // One extra element for sentinel
-			arr[length].prev = arr[length].next = sentinel = length; // Set up the sentinel
-		}
+			Contract.Requires<ArgumentOutOfRangeException>(0 <= length,
+				"Length must be a non-negative integer.");
+			Contract.Ensures(test == fn);
+			Contract.Ensures(Length == length);
 
-		/// <summary>
-		/// Determines whether one or more interesting values exist.
-		/// </summary>
-		/// <returns>
-		/// <c>true</c> if one or more values are interesting; otherwise, <c>false</c>.
-		/// </returns>
-		public bool IsInteresting() { return count > 0; }
+			test = fn;
+			arr = new Element[length + 1]; // One extra element for sentinel
+			arr[sentinel].prev = arr[sentinel].next = sentinel; // Set up the sentinel
+			for (int i = 0; i < Length; i++)
+			{
+				consider(i);
+			}
+		}
 
 		/// <summary>
 		/// Gets the index of an interesting element.
@@ -102,7 +140,18 @@ namespace PartitionableArray
 		/// <returns>
 		/// The index of an interesting element.
 		/// </returns>
-		public int AnyInteresting() { return arr[sentinel].next; }
+		public int AnyInteresting()
+		{
+			Contract.Requires<NullReferenceException>(Count > 0,
+				"The array must contain interesting elements.");
+			Contract.Ensures(0 <= Contract.Result<int>()
+				&& Contract.Result<int>() < Length,
+				"The returned index must be within the bounds of the array.");
+			Contract.Ensures(test(this[Contract.Result<int>()]),
+				"The returned index must be that of an interesting element.");
+
+			return arr[sentinel].next;
+		}
 
 		/// <summary>
 		/// Gets or sets the value at a certain index in the array.
@@ -110,45 +159,27 @@ namespace PartitionableArray
 		/// <param name='index'>
 		/// The index in the array.
 		/// </param>
-		public T this[int index] {
-			get {
-				if(!(0 <= index && index < length)) {
-					throw new IndexOutOfRangeException();
-				}
+		public T this[int index]
+		{
+			get
+			{
+				Contract.Requires<IndexOutOfRangeException>(
+					0 <= index && index < Length,
+					"Index must be within array bounds.");
+				Contract.Assume(index < this.arr.Length);
 
 				return arr[index].value;
 			}
-			set {
-				if(!(0 <= index && index < length)) {
-					throw new IndexOutOfRangeException();
-				}
+			set
+			{
+				Contract.Requires<IndexOutOfRangeException>(
+					0 <= index && index < Length,
+					"Index must be within array bounds.");
+				Contract.Assume(index < arr.Length);
 
 				arr[index].value = value;
 
-				// Update list of interesting elements
-				if(test(value)) { // Is interesting
-					if(!arr[index].interesting) { // Is not in list
-						// Add to list
-						count++;
-						arr[index].interesting = true;
-
-						arr[index].next = sentinel;
-						arr[index].prev = arr[sentinel].prev;
-						arr[arr[sentinel].prev].next = index;
-						arr[sentinel].prev = index;
-					}
-					// Else already in list, no need to add
-				} else { // Is not interesting
-					if(arr[index].interesting) { // Is in list
-						// Remove from list
-						count--;
-						arr[index].interesting = false;
-
-						arr[arr[index].prev].next = arr[index].next;
-						arr[arr[index].next].prev = arr[index].prev;
-					}
-					// Else is not already in list, no need to remove
-				}
+				consider(index); // Update list of interesting elements
 			}
 		}
 
--- C#/Test/Test.cs
+++ C#/Test/Test.cs
@@ -5,31 +5,34 @@ namespace Test
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-			PartitionableArray<int> arr = new PartitionableArray<int> ((val) => (val != 0 && val % 2 == 0), 20);
-
-			for (int i = 0; i < arr.Length; i++) {
-				arr [i] = i;
-				Console.WriteLine ("Added {0}.", arr [i]);
-				if (arr.IsInteresting ()) {
-					Console.WriteLine ("{0} is interesting.", arr [arr.AnyInteresting()]);
-				}
-			}
-
+			PartitionableArray<int> arr = new PartitionableArray<int>((val) => (/*val != 0 && */val % 2 == 0), 20);
 			Console.WriteLine("Array contains {0} interesting elements.", arr.Count);
-			Console.WriteLine ("Array contents:");
-			for (int i = 0; i < arr.Length; i++) {
-				Console.WriteLine ("{0}: {1}", i, arr [i]);
-			}
 
-			while (arr.IsInteresting()) {
-				int index = arr.AnyInteresting();
-				Console.WriteLine ("{0} is interesting. Setting equal to 0...", arr [index]);
-				arr [index] = 0;
+			for (int i = 1; i < arr.Length; i++)
+			{
+				arr[i] = i;
+				Console.WriteLine("Added {0}.", arr[i]);
 				Console.WriteLine("Array contains {0} interesting elements.", arr.Count);
 			}
-			Console.WriteLine("Array is {0}interesting.", arr.IsInteresting() ? "" : "not ");
+
+			Console.WriteLine("Array contents:");
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.WriteLine("{0}: {1}", i, arr[i]);
+			}
+
+			while (arr.Count > 0)
+			{
+				int index = arr.AnyInteresting();
+				Console.WriteLine("{0} is interesting. Setting equal to -1...", arr[index]);
+				arr[index] = -1;
+				Console.WriteLine("Array contains {0} interesting elements.", arr.Count);
+			}
+			Console.WriteLine("Array is {0}interesting.", arr.Count > 0 ? "" : "not ");
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
 		}
 	}
 }
